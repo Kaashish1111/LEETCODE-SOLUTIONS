c1de4fd45cf17547c371ec3266467522a8cf84b8
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-09-09 13:22:53 +0530

    Time: 2 ms (81.16%), Space: 7.4 MB (34.19%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int prev1 = 1, prev2 = 0, current;
+        
+        // Compute Fibonacci from 2 to n
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+};
