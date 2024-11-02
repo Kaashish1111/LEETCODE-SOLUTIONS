e179d6f6b0e9a5ee9dc73a90b018d9f034ad9a65
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-02 17:21:19 +0530

    Time: 0 ms (100%), Space: 10.1 MB (99.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,57 @@
+// classic approch
+
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         int count=0,count1=0,count2=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==0){
+//                 count++;
+//             }
+//             if(nums[i]==1){
+//                 count1++;
+//             }
+//             if(nums[i]==2){
+//                 count2++;
+//             }
+//         }
+//         for(int i=0;i<count;i++){
+//             nums[i]=0;
+//         }
+//         for(int i=count;i<count+count1;i++){
+//             nums[i]=1;
+//         }
+//         for(int i=count+count1;i<n;i++){
+//             nums[i]=2;
+//         }
+//     }
+// };
+
+
+// optimal approch ->Dutch National FLag Algo
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int count=0,count1=0,count2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                count++;
+        int n=nums.size();
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
             }
-            if(nums[i]==1){
-                count1++;
+            else if (nums[mid] == 1) {
+                mid++;
             }
-            if(nums[i]==2){
-                count2++;
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
-        for(int i=0;i<count;i++){
-            nums[i]=0;
-        }
-        for(int i=count;i<count+count1;i++){
-            nums[i]=1;
-        }
-        for(int i=count+count1;i<n;i++){
-            nums[i]=2;
-        }
+
     }
 };
