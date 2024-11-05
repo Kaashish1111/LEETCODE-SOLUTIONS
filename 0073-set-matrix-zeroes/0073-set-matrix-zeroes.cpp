a042d3d02c428bf1c79708bafbe787a51b45a178
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-05 20:50:20 +0530

    Time: 0 ms (100%), Space: 16.8 MB (83.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -44,35 +44,94 @@
 //    }
 // };
 
-// optimal approch
+// better approch
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+
+//         // Arrays to keep track of which rows and columns need to be zeroed
+//         vector<int> row(n, 0);
+//         vector<int> col(m, 0);
+
+//         // First pass: mark rows and columns that contain zeroes
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     row[i] = 1;
+//                     col[j] = 1;
+//                 }
+//             }
+//         }
+
+//         // Second pass: set matrix cells to zero if their row or column is marked
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (row[i] == 1 || col[j] == 1) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        // Arrays to keep track of which rows and columns need to be zeroed
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+        bool firstRowZero = false;
+        bool firstColZero = false;
 
-        // First pass: mark rows and columns that contain zeroes
+        // Check if the first row has any zeros
+        for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has any zeros
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and column as markers for rows and columns that should be zeroed
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        // Second pass: set matrix cells to zero if their row or column is marked
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] == 1 || col[j] == 1) {
+        // Set elements to zero based on markers in the first row and column
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        // Zero out the first row if needed
+        if (firstRowZero) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out the first column if needed
+        if (firstColZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
-
