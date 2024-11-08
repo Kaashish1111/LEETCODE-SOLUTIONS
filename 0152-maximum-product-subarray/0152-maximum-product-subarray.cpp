02d358dccc81ebbc2b7d8aac5f6d126624a1360d
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-08 22:21:08 +0530

    Time: 0 ms (100%), Space: 16.6 MB (38.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,14 +1,39 @@
+// brute force
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int n=nums.size();
+//         int maxi=INT_MIN;
+//         for(int i=0;i<n;i++){
+//             int product=1;
+//             for(int j=i;j<n;j++){
+//                 product *= nums[j];
+//                 maxi=max(maxi,product);
+//             }
+//         }
+//         return maxi;
+//     }
+// };
+
+// optimal approch 
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        int maxi=0;
+        int maxi=INT_MIN;
+        int pre=1;
+        int suff=1;
         for(int i=0;i<n;i++){
             int product=1;
-            for(int j=i;j<n;j++){
-                product *= nums[j];
-                maxi=max(maxi,product);
+            if(pre==0){
+                pre=1;
             }
+            if(suff==0){
+                suff=1;
+            }
+            pre=pre*nums[i];
+            suff=suff*nums[n-1-i];
+            maxi=max(maxi,max(pre,suff));
         }
         return maxi;
     }
