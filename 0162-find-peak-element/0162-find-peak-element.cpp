e1048bb3e39d7b947914fa671c089becbefe66ec
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2024-11-22 13:18:50 +0530

    Time: 0 ms (100%), Space: 11.6 MB (64.35%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -27,7 +27,7 @@ public:
             return n-1;
         }
         int low=1;
-        int high=n-1;
+        int high=n-2;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
