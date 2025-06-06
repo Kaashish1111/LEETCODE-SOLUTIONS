4dc27d082a0c568f905cd7a6c09e2ddb1856936c
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-06-06 16:04:12 +0530

    Time: 0 ms (100%), Space: 23.9 MB (49.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> arr(n, 0); // Create a vector with the same size and initialize with 0
-        int j = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                arr[j] = nums[i];
+        int n=nums.size();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
                 j++;
             }
         }
-
-        // Copy the result back to the original vector
-        for (int i = 0; i < n; i++) {
-            nums[i] = arr[i];
+        for(;j<n;j++){
+            nums[j]=0;
         }
+        
     }
 };
