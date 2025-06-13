fa086b4738128e9d7538f30c33b76aa32537a6a3
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-06-13 14:48:56 +0530

    Time: 10 ms (34.25%), Space: 17.2 MB (26.2%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows==1){
+        if(numRows==1 || s.length()<=numRows){
             return s;
         }
         vector<vector<char>> arr(numRows);
