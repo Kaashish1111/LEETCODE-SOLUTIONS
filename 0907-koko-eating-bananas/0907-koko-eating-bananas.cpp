69b2463980c71343e18dbf157d64234fce0fdd38
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-06-30 10:09:49 +0530

    Time: 9 ms (51.36%), Space: 23 MB (12.53%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,49 +1,24 @@
-// brute force approch
-// class Solution {
-// public:
-//     int minEatingSpeed(vector<int>& piles, int h) {
-//         int maxi = INT_MIN;
-//         int n = piles.size();
-        
-//         // Find the maximum pile size
-//         for (int i = 0; i < n; i++) {
-//             maxi = max(maxi, piles[i]);
-//         }
-        
-//         // Iterate over all possible speeds from 1 to maxi
-//         for (int k = 1; k <= maxi; k++) {
-//             int totalH = 0;
-//             for (int i = 0; i < n; i++) {
-//                 totalH += ceil((double)piles[i] / k);
-//             }
-//             if (totalH <= h) {
-//                 return k;
-//             }
-//         }
-        
-//         // Dummy return (shouldn't reach here due to constraints)
-//         return maxi;
-//     }
-// };
-
-
-// optimal approch
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int start = 1, end = *max_element(piles.begin(), piles.end());
-        int ans = end;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            long long total_times = 0;
-            for (int pile : piles) {
-                total_times += (pile + mid - 1) / mid; 
+        int n = piles.size();
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long sum = 0; // Use long long to prevent overflow
+
+            for (int i = 0; i < n; i++) {
+                sum += (piles[i] + mid - 1) / mid; 
+                // This is a neat trick to avoid using ceil
             }
-            if (total_times <= h) {
+
+            if (sum <= h) {
                 ans = mid;
-                end = mid - 1;
+                high = mid - 1;
             } else {
-                start = mid + 1;
+                low = mid + 1;
             }
         }
         return ans;
