6002e35ba1199dfb429f1a81654dc1ca28edcfe5
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-06-30 22:40:56 +0530

    Time: 680 ms (20.26%), Space: 295.5 MB (23.58%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
+    
+    void merge(int low, int mid, int high, vector<int>& nums) {
+        // You will write this later for merging two sorted halves
+        vector<int> temp;
+        int left=low;
+        int right=mid+1;
+        
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums[i]=temp[i-low];
+        }
+    }
+
+    void merge_help(int low, int high, vector<int>& nums) {
+        if (low == high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        
+        merge_help(low, mid, nums);
+        merge_help(mid + 1, high, nums);
+        merge(low, mid, high, nums);
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-    // int n= nums.size();
-    // int index = 0;
-    // for (int i = 0; i < n - 1; i++)
-    // {
-    //     index = i;
-    //     for (int j = i + 1; j < n; j++)
-    //     {
-    //         if (nums[j] < nums[index])
-    //         {
-    //             index = j;
-    //         }
-    //     }
-    //     swap(nums[index], nums[i]);
-    // }
-    // return nums;
-    sort(nums.begin(),nums.end());
-    return nums;
+        int n = nums.size();
+        merge_help(0, n - 1, nums);
+        return nums;
     }
 };
