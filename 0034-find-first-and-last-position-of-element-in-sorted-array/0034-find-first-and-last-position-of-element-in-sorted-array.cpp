308d3393e348bdd061cfb4b767db37d77767c84f
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-07-02 14:39:35 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0;
-        int end = n - 1;
-        int num1 = -1;
-        int num2 = -1;
-
-        // Binary search for the first occurrence (left boundary)
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                num1 = mid;
-                end = mid - 1;  // Continue searching to the left
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int n=nums.size(),left=0,right=n-1;
+        vector<int> ans(2,-1);
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                ans[0]=mid;
+                right=mid-1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
-
-        // Binary search for the last occurrence (right boundary)
-        start = 0;
-        end = n - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                num2 = mid;
-                start = mid + 1;  // Continue searching to the right
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        left=0,right=n-1;
+         while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                ans[1]=mid;
+                left=mid+1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
-
-        // Return the result as a vector
-        return {num1, num2};
+        return ans;
     }
 };
