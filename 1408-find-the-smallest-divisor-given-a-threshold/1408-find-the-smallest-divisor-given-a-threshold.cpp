ae9f7989a6860af04409d7c5dfa92b59ef84f012
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-07-02 23:18:18 +0530

    Time: 8 ms (61.63%), Space: 26.2 MB (16.37%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,45 +1,30 @@
-// brute force approch
-// class Solution {
-// public:
-//     int smallestDivisor(vector<int>& nums, int threshold) {
-//         int n=nums.size();
-//             int maxi = *max_element(nums.begin(), nums.end());
-
-//     //Find the smallest divisor:
-//     for (int d = 1; d <= maxi; d++) {
-//         //Find the summation result:
-//         int sum = 0;
-//         for (int i = 0; i < n; i++) {
-//             sum += ceil((double)(nums[i]) / (double)(d));
-//         }
-//         if (sum <= threshold)
-//             return d;
-//     }
-//     return -1;
-//     }
-// };
-
-
-// optimal approch
 class Solution {
+private:
+      // summation of division values 
+    int sumByD(vector<int>& nums, int limit) {
+        // Size of array
+        int n = nums.size(); 
+        // Find the summation of division values
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += ceil((double)(nums[i]) / (double)(limit));
+        }
+        return sum;
+    }
 public:
-bool can(vector<int>& nums, int threshold, int div){
-     int sum = 0;
-     for(auto i:nums){
-        sum +=ceil(i/(double)div);
-
-     }
-     return sum <= threshold;
-}
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int l =1, r= 1e6 , ans = 0;
-        while(l<=r){
-            int mid = l+((r-l)/2);
-            if(can(nums,threshold,mid)){
-                ans = mid;
-                r = mid - 1;
+        int high= *max_element(nums.begin(),nums.end());
+        int low=1;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(sumByD(nums, mid)<=threshold){
+                ans=mid;
+                high=mid-1;
             }
-            else l = mid + 1;
+            else{
+                low=mid+1;
+            }
         }
         return ans;
     }
