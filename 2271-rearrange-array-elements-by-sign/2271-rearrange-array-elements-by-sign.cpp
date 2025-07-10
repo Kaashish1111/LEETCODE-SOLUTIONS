cc9b2859bbaa2d5d48fe2643480ecc1e695d69ae
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-07-10 22:37:54 +0530

    Time: 1 ms (92.29%), Space: 128.1 MB (94.8%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,24 +1,44 @@
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         vector<int> positives;
+//         vector<int> negatives;
+
+//         // Separate positive and negative numbers
+//         for (int num : nums) {
+//             if (num > 0) {
+//                 positives.push_back(num);
+//             } else {
+//                 negatives.push_back(num);
+//             }
+//         }
+
+//         // Fill nums with alternating positives and negatives
+//         for(int i = 0; i < positives.size(); i++) {
+//             nums[2 * i] = positives[i];       // Place positive numbers at even indices
+//             nums[2 * i + 1] = negatives[i];    // Place negative numbers at odd indices
+//         }
+
+//         return nums;  // Return the rearranged array
+//     }
+// };
+
+// better optimised 
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> positives;
-        vector<int> negatives;
-
-        // Separate positive and negative numbers
-        for (int num : nums) {
-            if (num > 0) {
-                positives.push_back(num);
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        int pos = 0, neg = 1;
+        for (auto i : nums) {
+            if (i > 0) {
+                ans[pos] = i;
+                pos += 2;
             } else {
-                negatives.push_back(num);
+                ans[neg] = i;
+                neg += 2;
             }
         }
-
-        // Fill nums with alternating positives and negatives
-        for(int i = 0; i < positives.size(); i++) {
-            nums[2 * i] = positives[i];       // Place positive numbers at even indices
-            nums[2 * i + 1] = negatives[i];    // Place negative numbers at odd indices
-        }
-
-        return nums;  // Return the rearranged array
+        return ans;
     }
 };
