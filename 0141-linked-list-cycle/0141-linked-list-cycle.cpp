9a9a39619367ea8cd0a00958393dbac6e79600d5
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-08-29 18:42:19 +0530

    Time: 12 ms (38.62%), Space: 11.8 MB (53.17%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,34 +6,58 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//  brute force using hashmap
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//          // Initialize a pointer 'temp'
+//         // At the head of the linked list
+//         ListNode* temp = head;  
+
+//         // Create a map to keep track of
+//         // Encountered nodes
+//         std::unordered_map<ListNode*, int> nodeMap;  
+
+//         // Traverse the linked list
+//         while (temp != nullptr) {
+//             // If the node is already in the
+//             // Map, there is a loop
+//             if (nodeMap.find(temp) != nodeMap.end()) {
+//                 return true;
+//             }
+//             // Store the current node
+//             // In the map
+//             nodeMap[temp] = 1;
+            
+//             // Move to the next node
+//             temp = temp->next;  
+//         }
+
+//         // If the list is successfully traversed 
+//         // Without a loop, return false
+//         return false;
+//     }
+// };
+
+// optimal approch using 
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-         // Initialize a pointer 'temp'
-        // At the head of the linked list
-        ListNode* temp = head;  
+        if (!head || !head->next) return false;
 
-        // Create a map to keep track of
-        // Encountered nodes
-        std::unordered_map<ListNode*, int> nodeMap;  
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        // Traverse the linked list
-        while (temp != nullptr) {
-            // If the node is already in the
-            // Map, there is a loop
-            if (nodeMap.find(temp) != nodeMap.end()) {
-                return true;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;          // Move slow by 1
+            fast = fast->next->next;    // Move fast by 2
+
+            if (slow == fast) {
+                return true;  // Loop detected
             }
-            // Store the current node
-            // In the map
-            nodeMap[temp] = 1;
-            
-            // Move to the next node
-            temp = temp->next;  
         }
 
-        // If the list is successfully traversed 
-        // Without a loop, return false
-        return false;
+        return false; // No loop
     }
 };
