0339ec38539a577cf90b2995c584e7f0e4246b8d
Kashish Goyal <155823299+Kaashish1111@users.noreply.github.com>
2025-12-28 16:07:48 +0530

    Time: 0 ms (100%), Space: 21.7 MB (87.39%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // for each row
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
         for (int i = 0; i < 9; i++) {
-            vector<int> seen(9, 0); // vector<int> seen(size,value);
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.')
-                    continue;
-                int num = board[i][j] - '1';
-                if (seen[num])
-                    return false;
-                seen[num] = 1;
-            }
-        }
-        //  for each column
-        for (int i = 0; i < 9; i ++) {
-            vector<int> seen(9, 0); // vector<int> seen(size,value);
-            for (int j = 0; j < 9; j ++) {
-                if (board[j][i] == '.')
-                    continue;
-                int num = board[j][i] - '1';
-                if (seen[num])
-                    return false;
-                seen[num] = 1;
-            }
-        }
-        // for each box
-        // for each 3x3 box
-        for (int rowStart = 0; rowStart < 9; rowStart += 3) {
-            for (int colStart = 0; colStart < 9; colStart += 3) {
-                vector<int> seen(9, 0);
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        char c = board[rowStart + i][colStart + j];
-                        if (c == '.')
-                            continue;
-                        int num = c - '1';
-                        if (seen[num])
-                            return false;
-                        seen[num] = 1;
-                    }
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num])
+                        return false;
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[boxIndex][num] = true;
                 }
             }
         }
-
         return true;
     }
 };
